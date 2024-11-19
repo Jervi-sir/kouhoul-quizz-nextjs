849b34d06ba6bd32476cb49fe4dc6a451b04aff5
Jervi-sir <gacembekhira@gmail.com>
2024-11-19 21:56:53 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const rubik = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Frontend Quizz App",
-  description: "A frontend quiz app built with Next.js",
+  title: "Bac Quizz App",
+  description: "A Bac quiz app",
 };
 
 export default function RootLayout({
